d270b54cfa808e65c6a88b69a700918294a6e52f
dj-enby <deliafcipe@gmail.com>
2022-08-07 11:09:43 -0400

    added dont drop it, updated qsl

--- JellyPack_1.19.2/jelly.bat
+++ JellyPack_1.19.2/jelly.bat
@@ -1,35 +1,28 @@
-:: confirmed need update
+
 packwiz modrinth install capes
 ::packwiz modrinth install cull-less-leaves
 packwiz modrinth install quilt-loading-screen
-::official
 packwiz modrinth install smoothboot-fabric
 packwiz modrinth install starlight
-
-:: need install
 packwiz curseforge install carpet
 packwiz modrinth install cloth-config
 packwiz modrinth install c2me-fabric
-::official
 packwiz modrinth install continuity
 ::packwiz modrinth install debugify
 packwiz modrinth install ears
 packwiz modrinth install entityculling
 packwiz modrinth install fabric-language-kotlin
-::official
 packwiz curseforge install farsight-fabric
 packwiz modrinth install indium
 packwiz modrinth install iris
-::official
 packwiz modrinth install lazydfu
 ::packwiz modrinth install lithium
 packwiz modrinth install memoryleakfix
 packwiz modrinth install moreculling
 packwiz modrinth install ok-zoomer
 packwiz modrinth install qsl
-::official
 packwiz modrinth install sodium
-::official
+packwiz modrinth install dont-drop-it
 
 :: masa mods
 packwiz curseforge install malilib
