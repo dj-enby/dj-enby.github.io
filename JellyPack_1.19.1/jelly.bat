4fc998318f31c0b975f25e2ce826cbb6ffc68eed
dj-enby <deliafcipe@gmail.com>
2022-08-06 21:15:34 -0400

    updated to 1.0.3 and 1.1.1

---
+++ JellyPack_1.19.1/jelly.bat
@@ -0,0 +1,33 @@
+:: confirmed need update
+packwiz modrinth install capes
+::packwiz modrinth install cull-less-leaves
+packwiz modrinth install quilt-loading-screen
+packwiz modrinth install smoothboot-fabric
+packwiz modrinth install starlight
+
+:: need install
+packwiz curseforge install carpet
+packwiz modrinth install cloth-config
+packwiz modrinth install c2me-fabric
+packwiz modrinth install continuity
+packwiz modrinth install debugify
+packwiz modrinth install ears
+packwiz modrinth install entityculling
+packwiz modrinth install fabric-language-kotlin
+packwiz curseforge install farsight-fabric
+packwiz modrinth install indium
+packwiz modrinth install iris
+packwiz modrinth install lazydfu
+packwiz modrinth install lithium
+packwiz modrinth install memoryleakfix
+packwiz modrinth install moreculling
+packwiz modrinth install ok-zoomer
+packwiz modrinth install qsl
+packwiz modrinth install sodium
+
+:: masa mods
+packwiz curseforge install malilib
+packwiz curseforge install minihud
+packwiz curseforge install item-scroller
+packwiz curseforge install litematica
+packwiz curseforge install tweakeroo
---
+++ JellyPack_1.19.2/jelly.bat
@@ -0,0 +1,39 @@
+:: confirmed need update
+packwiz modrinth install capes
+::packwiz modrinth install cull-less-leaves
+packwiz modrinth install quilt-loading-screen
+::official
+packwiz modrinth install smoothboot-fabric
+packwiz modrinth install starlight
+
+:: need install
+packwiz curseforge install carpet
+packwiz modrinth install cloth-config
+packwiz modrinth install c2me-fabric
+::official
+packwiz modrinth install continuity
+::packwiz modrinth install debugify
+packwiz modrinth install ears
+packwiz modrinth install entityculling
+packwiz modrinth install fabric-language-kotlin
+::official
+packwiz curseforge install farsight-fabric
+packwiz modrinth install indium
+packwiz modrinth install iris
+::official
+packwiz modrinth install lazydfu
+::packwiz modrinth install lithium
+packwiz modrinth install memoryleakfix
+packwiz modrinth install moreculling
+packwiz modrinth install ok-zoomer
+packwiz modrinth install qsl
+::official
+packwiz modrinth install sodium
+::official
+
+:: masa mods
+packwiz curseforge install malilib
+packwiz curseforge install minihud
+packwiz curseforge install item-scroller
+packwiz curseforge install litematica
+::packwiz curseforge install tweakeroo
