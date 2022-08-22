143154dcfdfa04026227d39198ee2e5c45c3caee
dj-enby <deliafcipe@gmail.com>
2022-08-21 20:03:18 -0400

    removed minihud and litematica schems, updated mods

--- JellyPack_1.19.2/jelly.bat
+++ JellyPack_1.19.2/jelly.bat
@@ -8,7 +8,7 @@ packwiz curseforge install carpet
 ::packwiz modrinth install cloth-config
 packwiz modrinth install c2me-fabric
 packwiz modrinth install continuity
-::packwiz modrinth install debugify
+packwiz modrinth install debugify
 packwiz modrinth install ears
 packwiz modrinth install entityculling
 packwiz modrinth install fabric-language-kotlin
@@ -17,8 +17,8 @@ packwiz modrinth install indium
 packwiz modrinth install iris
 packwiz modrinth install lazydfu
 packwiz modrinth install lithium
-packwiz modrinth install memoryleakfix
-packwiz modrinth install moreculling
+::packwiz modrinth install memoryleakfix
+::packwiz modrinth install moreculling fxmorin mods, removing
 packwiz modrinth install ok-zoomer
 packwiz modrinth install qsl
 packwiz modrinth install sodium
